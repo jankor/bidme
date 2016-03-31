5e35cf6de071b963fc4eb6fe2223ab59d63938ed
Jan Korenek <jan.korenek@deliveryhero.com>
2016-03-31 17:24:41 +0200

    make iOS buildable

--- src/native/index.js
+++ src/native/index.js
@@ -2,25 +2,25 @@ import 'react-native-browser-polyfill';
 import App from './app/App.react';
 import Component from 'react-pure-render/component';
 import React, { AppRegistry, Platform } from 'react-native';
-import config from './config';
+//import config from './config';
 import configureStore from '../common/configureStore';
 import createEngine from 'redux-storage-engine-reactnativeasyncstorage';
-import messages from './messages';
+//import messages from './messages';
 import { Provider } from 'react-redux';
 
-require('../server/intl/polyfillLocales')(self, config.locales);
+require('../server/intl/polyfillLocales')(self, ['En']);
 
 export default function index() {
   const initialState = {
     config: {
-      appName: config.appName,
-      firebaseUrl: config.firebaseUrl
+      appName: 'bidme',
+      firebaseUrl: 'https://test.firebaseio.com'
     },
     intl: {
       // TODO: Detect native current locale.
-      currentLocale: config.defaultLocale,
-      locales: config.locales,
-      messages
+      currentLocale: 'En',
+      locales: ['En'],
+      messages: {}
     },
     device: {
       platform: Platform.OS
